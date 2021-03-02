8935425a3d6540a358191fda9f3ad5d050719354
Louafi Noureddine <noureddinelouafi8@gmail.com>
2021-03-02 19:21:13 +0100

    Auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { secretKey } from "../config.js";
+
+export const auth = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  let decoded;
+  if (!token) res.status(401).json({ message: "No token" });
+  try {
+    decoded = jwt.verify(token, secretKey);
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
